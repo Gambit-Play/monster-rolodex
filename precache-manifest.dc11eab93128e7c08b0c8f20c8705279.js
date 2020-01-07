c15c9dc2193f7ed3420318bc116414f582830501
Anuar Abdullahi <anuar.abdullahi@kinetit.com>
2020-01-07 01:17:01 +0000

    Updates

---
+++ precache-manifest.dc11eab93128e7c08b0c8f20c8705279.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7cac7549a9be3d506c9a2a417ddefeb",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "adfc334658d4b8f2690a",
+    "url": "/monster-rolodex/static/css/main.7aa0a2cd.chunk.css"
+  },
+  {
+    "revision": "c728ba4a2366e885ec54",
+    "url": "/monster-rolodex/static/js/2.d8d20882.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.d8d20882.chunk.js.LICENSE"
+  },
+  {
+    "revision": "adfc334658d4b8f2690a",
+    "url": "/monster-rolodex/static/js/main.3fa6113a.chunk.js"
+  },
+  {
+    "revision": "cd391451e66ac7af83df",
+    "url": "/monster-rolodex/static/js/runtime-main.1356d2e6.js"
+  }
+]);
